e7612d39609084d4e23c3dcd9e9ab363914fc8a6
timrose1234 <timrose1234@gmail.com>
2013-10-20 16:41:01 -0400

    Added Main Menu

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	DontDestroyOnLoad(this.gameObject);
+		
+	  Application.LoadLevel(1);
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(  (Application.loadedLevel)== 2)
+		{
+			
+	Destroy(this.gameObject);		
+		
+			
+		}
+		
+		
+	}
+}
